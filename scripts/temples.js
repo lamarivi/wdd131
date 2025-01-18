bab47ccd304a27bfced99286c4c8ee9606b1ffb5
lamarivi <85804476+lamarivi@users.noreply.github.com>
2025-01-18 14:23:56 -0400

    Add files via upload

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
+
+const hamburger = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburguer.classList.remove("active");
+}))
